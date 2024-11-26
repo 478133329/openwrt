7ac83eff3d29d19bddb58ea4d28a2a6dbdba2742
sijie.wang <sijie.wang@sophgo.com>
2024-11-26 18:24:26 +0800

    [fix](openwrt):fix compile error while using high version external toolchain, add sophgo config. [description ]:NA [root   cause]:NA [JIRA      ID]:NA [chip project]:NA [side effects]:NA [Affected *.ko/*.so/*.a] :NA
    Change-Id: I9054a90e1cf191dadad73548d85a85fa1de39669

---
+++ target/linux/sophgo/bm1688/target.mk
@@ -0,0 +1,8 @@
+ARCH:=aarch64
+SUBTARGET:=bm1688
+BOARDNAME:=bm1688_wevb_emmc
+
+define Target/Description
+	Build firmware image for bm1688_wevb_emmc.
+	This firmware features a 64 bit kernel.
+endef
---
+++ target/linux/sophgo/cv186ah/target.mk
@@ -0,0 +1,8 @@
+ARCH:=aarch64
+SUBTARGET:=cv186ah
+BOARDNAME:=cv186ah_wevb_emmc
+
+define Target/Description
+	Build firmware image for cv186ah_wevb_emmc.
+	This firmware features a 64 bit kernel.
+endef
